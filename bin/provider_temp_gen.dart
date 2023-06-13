6badc8ba06604b5449a529e687b041ff2ec45831
cheng <511928426@qq.com>
2023-06-13 18:07:10 +0800

    删除没用到的代码

--- bin/provider_temp_gen.dart
+++ bin/provider_temp_gen.dart
@@ -7,15 +7,6 @@
  * @FilePath: \provider_temp_gen\bin\provider_temp_gen.dart
  * @ObjectDescription: 
  */
-/*
- * @Author: cheng
- * @Version: 1.0
- * @Date: 2023-04-25 13:04:14
- * @LastEditors: cheng
- * @LastEditTime: 2023-05-08 13:58:33
- * @FilePath: \provider_temp_gen\bin\provider_temp_gen.dart
- * @ObjectDescription: 
- */
 import 'dart:io';
 
 import 'package:args/args.dart';
--- test/provider_temp_gen_test.dart
+++ test/provider_temp_gen_test.dart
@@ -1,4 +1,12 @@
-import 'package:provider_temp_gen/provider_temp_gen.dart';
+/*
+ * @Author: cheng
+ * @Version: 1.0
+ * @Date: 2023-04-25 13:04:14
+ * @LastEditors: cheng
+ * @LastEditTime: 2023-06-13 18:04:52
+ * @FilePath: \provider_temp_gen\test\provider_temp_gen_test.dart
+ * @ObjectDescription: 
+ */
 import 'package:test/test.dart';
 
 void main() {
